46192aecfa84958075b84b27b9bf0eab440f07d3
MathisDelrue51 <mathis.delrue@gmail.com>
2021-01-06 19:25:41 +0100

    En avant

--- js/app.js
+++ js/app.js
@@ -37,7 +37,6 @@ var app = {
         } else if (lineIndex == app.player.positionY && columnIndex == app.player.positionX) {
           var player = document.createElement('div');
           player.classList.add('player');
-          player.classList.add('player--right');
           square.appendChild(player);
         }
 
@@ -84,26 +83,76 @@ var app = {
   },
 
   turnRight: function () {
+    var player = document.querySelector('.player');
     switch (app.player.direction) {
       case 'right':
         app.player.direction = 'down';
         player.classList.remove('player--right');
-        player.classList.add('player--down');        break;
+        player.classList.add('player--down');
+        break;
+
       case 'down':
         app.player.direction = 'left';
         player.classList.remove('player--down');
-        player.classList.add('player--left');        break;
+        player.classList.add('player--left');
+        break;
+
       case 'left':
         app.player.direction = 'up';
         player.classList.remove('player--left');
-        player.classList.add('player--up');        break;
+        player.classList.add('player--up');
+        break;
+
       case 'up':
         app.player.direction = 'right';
         player.classList.remove('player--up');
-        player.classList.add('player--right');        break;
+        player.classList.add('player--right');
+        break;
     }
   },
 
+  moveForward: function () {
+    //SI la direction du joueur est droite ou gauche on va modifier x sinon on modifie y
+    switch (app.player.direction) {
+      case 'right':
+        if (app.player.positionX < app.nbColumn) {
+          app.player.positionX = app.player.positionX + 1;
+          console.log(app.player.positionX);
+        } else {
+          console.log("Je ne peux pas aller plus loin dans cette direction");
+        }
+        break;
+
+      case 'down':
+        if (app.player.positionY < app.nbRow) {
+          app.player.positionY = app.player.positionY + 1;
+          console.log(app.player.positionY);
+        } else {
+          console.log("Je ne peux pas aller plus loin dans cette direction");
+        }
+        break;
+
+      case 'left':
+        if (app.player.positionX > 0) {
+          app.player.positionX = app.player.positionX - 1;
+          console.log(app.player.positionX);
+        }else {
+          console.log("Je ne peux pas aller plus loin dans cette direction");
+        }
+        break;
+        
+      case 'up':
+        if (app.player.positionY > 0) {
+          app.player.positionY = app.player.positionY - 1;
+          console.log(app.player.positionY);
+        }else {
+          console.log("Je ne peux pas aller plus loin dans cette direction");
+        }
+        break;
+    }
+
+  },
 };
+
 
 document.addEventListener('DOMContentLoaded', app.init);
