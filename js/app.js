8794f9cbc024ee37fa0cb2ec3607cfc948d8d931
MathisDelrue51 <mathis.delrue@gmail.com>
2021-01-06 18:42:26 +0100

    Rotation du joueur

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var app = {
     positionX: 0,
     positionY: 0,
     direction: 'right',
+    // image: document.querySelector('.player'),
   },
   targetCell: {
     positionX: 5,
@@ -12,6 +13,7 @@ var app = {
   nbColumn: 6,
 
   grid: document.getElementById('board'),
+
 
   init: function () {
     console.log('init !');
@@ -35,25 +37,73 @@ var app = {
         } else if (lineIndex == app.player.positionY && columnIndex == app.player.positionX) {
           var player = document.createElement('div');
           player.classList.add('player');
+          player.classList.add('player--right');
           square.appendChild(player);
         }
 
         line.appendChild(square);
       }
     }
-
-
-
   },
 
   clearBoard: function () {
     app.grid.textContent = '';
   },
 
-  redrawBoard :function(){
+  redrawBoard: function () {
     app.clearBoard();
     app.drawBoard();
-  }
+  },
+
+  turnLeft: function () {
+    var player = document.querySelector('.player');
+    switch (app.player.direction) {
+      case 'right':
+        app.player.direction = 'up';
+        // document.querySelector('.player').style.transform = 'rotate(-90deg)';
+        player.classList.remove('player--right');
+        player.classList.add('player--up');
+        break;
+      case 'up':
+        app.player.direction = 'left';
+        player.classList.remove('player--up');
+        player.classList.add('player--left');
+        break;
+      case 'left':
+        app.player.direction = 'down';
+        player.classList.remove('player--left');
+        player.classList.add('player--down');
+        break;
+      case 'down':
+        app.player.direction = 'right';
+        player.classList.remove('player--down');
+        player.classList.add('player--right');
+        break;
+    }
+
+  },
+
+  turnRight: function () {
+    switch (app.player.direction) {
+      case 'right':
+        app.player.direction = 'down';
+        player.classList.remove('player--right');
+        player.classList.add('player--down');        break;
+      case 'down':
+        app.player.direction = 'left';
+        player.classList.remove('player--down');
+        player.classList.add('player--left');        break;
+      case 'left':
+        app.player.direction = 'up';
+        player.classList.remove('player--left');
+        player.classList.add('player--up');        break;
+      case 'up':
+        app.player.direction = 'right';
+        player.classList.remove('player--up');
+        player.classList.add('player--right');        break;
+    }
+  },
+
 };
 
 document.addEventListener('DOMContentLoaded', app.init);
