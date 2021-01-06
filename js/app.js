ceed6adf6baa7e2985538550f95040c15ee0c0a1
MathisDelrue51 <mathis.delrue@gmail.com>
2021-01-06 17:03:46 +0100

    on peut bouger le personnage avec la console

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 var app = {
-  positionX: 0,
-  positionY: 0,
-  direction: 'right',
+  player: {
+    positionX: 0,
+    positionY: 0,
+    direction: 'right',
+  },
   targetCell: {
-    x: 5,
-    y: 3,
+    positionX: 5,
+    positionY: 3,
   },
   nbRow: 4,
   nbColumn: 6,
+
+  grid: document.getElementById('board'),
 
   init: function () {
     console.log('init !');
@@ -19,16 +23,37 @@ var app = {
     for (lineIndex = 0; lineIndex < app.nbRow; lineIndex++) {
       var line = document.createElement('div');
       line.className = 'line';
-      var grid = document.getElementById('board');
-      grid.appendChild(line);
-      
-      for (columnIndex = 0; columnIndex < app.nbColumn; columnIndex++){
+      app.grid.appendChild(line);
+
+      for (columnIndex = 0; columnIndex < app.nbColumn; columnIndex++) {
         var square = document.createElement('div');
-        square.className = 'square';        
+        square.className = 'square';
+
+        if (lineIndex == app.targetCell.positionY && columnIndex == app.targetCell.positionX) {
+          square.classList.add('square--targetCell');
+
+        } else if (lineIndex == app.player.positionY && columnIndex == app.player.positionX) {
+          var player = document.createElement('div');
+          player.classList.add('player');
+          square.appendChild(player);
+        }
+
         line.appendChild(square);
       }
     }
+
+
+
   },
+
+  clearBoard: function () {
+    app.grid.textContent = '';
+  },
+
+  redrawBoard :function(){
+    app.clearBoard();
+    app.drawBoard();
+  }
 };
 
 document.addEventListener('DOMContentLoaded', app.init);
