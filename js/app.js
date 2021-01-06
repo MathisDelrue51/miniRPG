7b94affaa7073005ea448e4c8a1cbb2a18dc365f
MathisDelrue51 <mathis.delrue@gmail.com>
2021-01-06 16:30:54 +0100

    Ajout de la grille

--- js/app.js
+++ js/app.js
@@ -1,7 +1,34 @@
 var app = {
+  positionX: 0,
+  positionY: 0,
+  direction: 'right',
+  targetCell: {
+    x: 5,
+    y: 3,
+  },
+  nbRow: 4,
+  nbColumn: 6,
+
   init: function () {
     console.log('init !');
-  }
+    app.drawBoard();
+  },
+
+  drawBoard: function () {
+
+    for (lineIndex = 0; lineIndex < app.nbRow; lineIndex++) {
+      var line = document.createElement('div');
+      line.className = 'line';
+      var grid = document.getElementById('board');
+      grid.appendChild(line);
+      
+      for (columnIndex = 0; columnIndex < app.nbColumn; columnIndex++){
+        var square = document.createElement('div');
+        square.className = 'square';        
+        line.appendChild(square);
+      }
+    }
+  },
 };
 
 document.addEventListener('DOMContentLoaded', app.init);
